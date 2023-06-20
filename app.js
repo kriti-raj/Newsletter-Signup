6b182c3182c514081a3bb5b51feb3ba0b5790647
Kriti Raj <6517kritiraj@gmail.com>
2023-06-21 05:29:14 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,78 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function (req, res) {
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us21.api.mailchimp.com/3.0/lists/a9ecb7605b1";
+
+    const options = {
+        method: "POST",
+        auth: "kritiraj:2d2cdc03bdc47f2a9825384621dbb3b1-us21"
+
+    }
+
+    const request = https.request(url, options, function (response) {
+
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        }
+        else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
+
+
+});
+
+app.post("/failure", function (req, res) {
+    res.redirect("/");
+})
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
+});
+
+// API Key
+// 2d2cdc03bdc47f2a9825384621dbb3b1-us21
+
+// List id
+// a9ecb7605b
